1f6b4f3a658890d286b88a9c8f65ad55f189371d
George Garcia <garcia.george.b@gmail.com>
2019-12-30 20:30:47 -0800

    Initial Commit

---
+++ GHub Squad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHub Squad
+//
+//  Created by George Garcia on 12/30/19.
+//  Copyright © 2019 GeeTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
